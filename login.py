65ebb205181f145279b5f864fbed59be27672cd4
pasuva <31448608+pasuva@users.noreply.github.com>
2025-02-19 13:59:52 +0100

    Add files via upload

---
+++ login.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import sqlite3
+import bcrypt
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "../data/usuarios.db")
+
+
+def verify_user(nombre, password):
+    """ Verifica si un usuario existe y su contraseña es correcta """
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute("SELECT password, role FROM usuarios WHERE username = ?", (nombre,))
+    result = cursor.fetchone()
+    conn.close()
+
+    if result:
+        hashed_pw, rol = result
+        if bcrypt.checkpw(password.encode(), hashed_pw.encode()):
+            return rol  # Retornamos el rol si la autenticación es correcta
+    return None
+
+
+def login():
+    """ Pantalla de inicio de sesión """
+    # Mostrar el ícono de usuario centrado y más grande en la barra lateral
+    st.markdown("""
+            <style>
+                .user-circle {
+                    width: 100px;
+                    height: 100px;
+                    border-radius: 50%;
+                    background-color: #0073e6;
+                    color: white;
+                    font-size: 50px;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    margin-bottom: 30px;
+                    text-align: center;
+                    margin-left: auto;
+                    margin-right: auto;
+                }
+            </style>
+            <div class="user-circle">👤</div>
+            """, unsafe_allow_html=True)
+    st.title("🔐 Inicio de sesión")
+
+    nombre = st.text_input("Usuario")
+    password = st.text_input("Contraseña", type="password")
+
+    if st.button("Iniciar sesión"):
+        rol = verify_user(nombre, password)
+        if rol:
+            st.session_state["logged_in"] = True
+            st.session_state["username"] = nombre
+            st.session_state["role"] = rol
+            st.success(f"Bienvenido, {nombre} ({rol})")
+            st.rerun()
+        else:
+            st.error("Usuario o contraseña incorrectos")
+
+
+if __name__ == "__main__":
+    login()
