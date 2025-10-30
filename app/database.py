379c8cf1734b66dbaf38bb752fe3b75430939401
pasuva <31448608+pasuva@users.noreply.github.com>
2025-10-30 09:55:02 +0100

    Delete app directory

--- app/database.py
+++
@@ -1,7 +0,0 @@
-from sqlmodel import SQLModel, create_engine
-
-DATABASE_URL = "sqlite:///./dev.db"
-engine = create_engine(DATABASE_URL, echo=True)
-
-def init_db():
-    SQLModel.metadata.create_all(engine)
