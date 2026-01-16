2040ec4e9c14131af85da73967a11f5ad577a692
pasuva <31448608+pasuva@users.noreply.github.com>
2026-01-16 15:02:49 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -81,6 +81,8 @@ else:
         demo.demo_dashboard()
     elif rol == "tecnico":
         perfil_tecnico.tecnico_dashboard()
+    elif rol == "marketing":
+        marketing.marketing_dashboard()
     else:
         st.error("Rol no reconocido")
 
